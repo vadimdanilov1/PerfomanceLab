3f6c27f01b4dd14cf2352e4cc5b75c81f9cc6f68
Vadim Danilov <99761501+vadimdanilov1@users.noreply.github.com>
2022-09-10 18:19:32 +0400

    Add files via upload

--- task1/Task1.java
+++ task1/Task1.java
@@ -3,20 +3,41 @@ import java.util.ArrayList;
 public class Task1 {
     public static void main(String[] args) {
 
-      int n = Integer.parseInt(args[0]);
-      int m = Integer.parseInt(args[1]);
-      
-      ArrayList<Integer> way = new ArrayList<>();
+        int n = Integer.parseInt(args[0]);
+		int m = Integer.parseInt(args[1]);
 
-      int i = 1;
-      while (true){
-          way.add(i);
-          i = 1 + (i + m - 2) % n;
-          if (i == 1)
-              break;
-      }
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[m];
+        ArrayList<Integer> way = new ArrayList<>();
 
-        System.out.println(way);
+        for (int i = 0; i < n; i++) {
+            arr1[i] = i + 1;
+        }
 
+        int j = 1;
+        while (true) {
+
+                j--;
+            for (int i = 0; i < m; i++) {
+                arr2[i] = arr1[j];
+
+                if (j == m && i != m - 1)
+                    j = 0;
+                else j++;
+            }
+            way.add(arr2[0]);
+
+            if (arr2[m - 1] == 1)
+                break;
+        }
+
+        String str = "";
+
+        for (int i = 0; i < way.size(); i++) {
+            str = str + way.get(i);
+        }
+
+        System.out.println(str);
+  
     }
 }
